66d9ca7e327297a4ca1e06d5cb993959c826b65d
anacolell <anacolell8@gmail.com>
2022-11-16 18:25:34 +0200

    Updates skills

--- src/components/Skills/SkillsData.js
+++ src/components/Skills/SkillsData.js
@@ -1,21 +1,20 @@
 import {
   FaReact,
-  FaHtml5,
   FaCss3,
   FaJs,
   FaGitAlt,
   FaMobileAlt,
+  FaFigma,
 } from "react-icons/fa";
-import { SiRails } from "react-icons/si";
-import { DiRuby } from "react-icons/di";
+import { SiTypescript, SiStorybook } from "react-icons/si";
 
 export const skillsData = [
-  { id: 1, name: "HTML", logo: <FaHtml5 /> },
-  { id: 2, name: "CSS", logo: <FaCss3 /> },
+  { id: 1, name: "React", logo: <FaReact /> },
+  { id: 2, name: "Typescript", logo: <SiTypescript /> },
   { id: 3, name: "JavaScript", logo: <FaJs /> },
-  { id: 4, name: "React", logo: <FaReact /> },
-  { id: 5, name: "Ruby", logo: <DiRuby /> },
-  { id: 6, name: "Rails", logo: <SiRails /> },
+  { id: 4, name: "Storybook", logo: <SiStorybook /> },
+  { id: 5, name: "CSS", logo: <FaCss3 /> },
+  { id: 6, name: "Figma", logo: <FaFigma /> },
   { id: 7, name: "Git", logo: <FaGitAlt /> },
   { id: 8, name: "Responsive", logo: <FaMobileAlt /> },
 ];
